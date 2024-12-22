32874e7b72a866198ede7351beb4ace54a47f3cc
davidshevnya <dawidshevnya@gmail.com>
2024-12-22 18:00:10 +0200

    update readme.md

---
+++ readme/dynamics_online/server-data.php
@@ -0,0 +1,9 @@
+<?php
+list($host, $port) = explode(":", "185.169.134.43:7777"); // ÏÎÑÒÀÂÜÒÅ ÑÂÎÉ ÀÉÄÈ!!
+$query = new Query($host, $port);
+$query->connect();
+$players = $query->getInformation()['players'];
+$max_players = $query->getInformation()['max_players'];
+$serverName = "Arizona TEST";
+echo '{"arizona":[{"number":1,"name":"'.$serverName.'","ip":"'.$host.'","port":'.$port.',"online":'.$players.',"maxplayers":'.$max_players.',"password":false}]}';
+?>
